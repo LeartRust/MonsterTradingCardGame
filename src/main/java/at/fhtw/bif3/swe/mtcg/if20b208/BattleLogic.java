bad7f45888ec97b71c51376d68c01fa65704009d
Leart <leartr99@hotmail.com>
2021-12-30 15:56:57 +0100

    check specialities

--- src/main/java/at/fhtw/bif3/swe/mtcg/if20b208/BattleLogic.java
+++ src/main/java/at/fhtw/bif3/swe/mtcg/if20b208/BattleLogic.java
@@ -1,9 +1,6 @@
 package at.fhtw.bif3.swe.mtcg.if20b208;
 
-import at.fhtw.bif3.swe.mtcg.if20b208.cards.Card;
-import at.fhtw.bif3.swe.mtcg.if20b208.cards.ElementType;
-import at.fhtw.bif3.swe.mtcg.if20b208.cards.MonsterCard;
-import at.fhtw.bif3.swe.mtcg.if20b208.cards.SpellCard;
+import at.fhtw.bif3.swe.mtcg.if20b208.cards.*;
 import at.fhtw.bif3.swe.mtcg.if20b208.user.Deck;
 import at.fhtw.bif3.swe.mtcg.if20b208.user.User;
 
@@ -41,9 +38,11 @@ public class BattleLogic {
         } else if (user2.getDeck().size() == 0){
             System.out.println(user2.getUsername() + " has no deck to compete with");
         }else {
-            for(int i = 0; i < 100; i++){
+            for(int i = 1; i < 101; i++){
                 //get random card from both decks and compare
-                if (user1.getDeck().size() != 0 && user2.getDeck().size() != 0){
+                if (i == 100){
+                    System.out.println("This Match ended in a Draw");
+                } else if (user1.getDeck().size() != 0 && user2.getDeck().size() != 0){
                     compareCards(i);
                 }else if(user1.getDeck().size() == 0) {
                     System.out.println("User 2 Won");
@@ -53,13 +52,12 @@ public class BattleLogic {
                     System.out.println("User 1 Won");
                     System.out.println("User 1 Deck size: " + user1.getDeck().size());
                     break;
-                }else {
-                    System.out.println("This Match ended in a Draw");
                 }
             }
         }
 
         //Reset Swapped cards to initial status
+        //TODO reset Deck (when Deck choosing is updated)
         user1.setDeck(Deck.createDeck(user1));
         user2.setDeck(Deck.createDeck(user2));
         System.out.println("========================================");
@@ -75,13 +73,10 @@ public class BattleLogic {
         Card card1 = user1.getDeck().get(user1DrawnCard);
         Card card2 = user2.getDeck().get(user2DrawnCard);
 
-        if(card1 instanceof MonsterCard){
-            System.out.println(((MonsterCard) card1).getMonsterType());
-        }
-
         //TODO Method to compare all specialities!
+        if (checkSpecialities(card1, card2, user1DrawnCard, user2DrawnCard) == true){
 
-        if(user1.getDeck().get(user1DrawnCard) instanceof SpellCard || user2.getDeck().get(user2DrawnCard) instanceof SpellCard) {
+        } else if(user1.getDeck().get(user1DrawnCard) instanceof SpellCard || user2.getDeck().get(user2DrawnCard) instanceof SpellCard) {
             compareByElements(card1, card2, user1DrawnCard, user2DrawnCard);
         }else if(card1 instanceof MonsterCard && card2 instanceof MonsterCard){
             compareDMG(card1.getDAMAGE(), card2.getDAMAGE(), user1DrawnCard, user2DrawnCard);
@@ -90,10 +85,6 @@ public class BattleLogic {
     }
 
     private void compareByElements(Card card1, Card card2, int user1DrawnCard, int user2DrawnCard){
-
-        //println only for testing
-        System.out.println(card1.getName() + " " + card1.getDAMAGE() + " ELEMENT----------------------------");
-        System.out.println(card2.getName() + " " + card2.getDAMAGE() + " ELEMENT----------------------------");
 
         if((card1.getElement() == ElementType.FIRE) && (card2.getElement() == ElementType.NORMAL)){
             compareDMG((card1.getDAMAGE() * 2), (card2.getDAMAGE() / 2), user1DrawnCard, user2DrawnCard);
@@ -127,13 +118,65 @@ public class BattleLogic {
 
     }
 
-    private void compareMonsterFight(MonsterCard card1, MonsterCard card2){
-        System.out.println("COMPARE ONLY MONSTER CARDS");
-        System.out.println(card1.getName() + " tpy: " + card1.getMonsterType());
-        System.out.println(card2.getName() + " tpy: " + card2.getMonsterType());
+    private boolean checkSpecialities(Card card1, Card card2, int user1DrawnCard, int user2DrawnCard){
+        System.out.println("COMPARE SPECIALITIES");
+
+        if (card1 instanceof MonsterCard && card2 instanceof MonsterCard){
+            //check if Goblin vs Dragon
+            if (((MonsterCard) card1).getMonsterType().equals(MonsterType.GOBLIN) && ((MonsterCard) card2).getMonsterType().equals(MonsterType.DRAGON)){
+                compareDMG(0,card2.getDAMAGE(), user1DrawnCard, user2DrawnCard);
+                System.out.println("Dragons win against Goblins");
+                return true;
+            } else if (((MonsterCard) card2).getMonsterType().equals(MonsterType.GOBLIN) && ((MonsterCard) card1).getMonsterType().equals(MonsterType.DRAGON)){
+                compareDMG(card1.getDAMAGE(),0, user1DrawnCard, user2DrawnCard);
+                System.out.println("Dragons win against Goblins");
+                return true;
+            }
+            //check if Wizard vs Ork
+            else if (((MonsterCard) card1).getMonsterType().equals(MonsterType.WIZARD) && ((MonsterCard) card2).getMonsterType().equals(MonsterType.ORK)){
+                compareDMG(card1.getDAMAGE(),0, user1DrawnCard, user2DrawnCard);
+                System.out.println("Wizards win against Orks");
+                return true;
+            }else if(((MonsterCard) card2).getMonsterType().equals(MonsterType.WIZARD) && ((MonsterCard) card1).getMonsterType().equals(MonsterType.ORK)){
+                compareDMG(0, card2.getDAMAGE(), user1DrawnCard, user2DrawnCard);
+                System.out.println("Wizards win against Orks");
+                return true;
+            }
+            //check if FireElves vs Dragon
+            else if((((MonsterCard) card1).getMonsterType().equals(MonsterType.ELF) && card1.getElement().equals(ElementType.FIRE)) && ((MonsterCard) card2).getMonsterType().equals(MonsterType.DRAGON)){
+                compareDMG(card1.getDAMAGE(), 0, user1DrawnCard, user2DrawnCard);
+                System.out.println("FireElves win against Dragons");
+                return true;
+            }else if((((MonsterCard) card2).getMonsterType().equals(MonsterType.ELF) && card2.getElement().equals(ElementType.FIRE)) && ((MonsterCard) card1).getMonsterType().equals(MonsterType.DRAGON)){
+                compareDMG(0, card2.getDAMAGE(), user1DrawnCard, user2DrawnCard);
+                System.out.println("FireElves win against Dragons");
+                return true;
+            }
+        }
+        //check if Water-Spell vs Knight
+        else if((card1 instanceof SpellCard && card1.getElement().equals(ElementType.WATER)) && (card2 instanceof MonsterCard && ((MonsterCard) card2).getMonsterType().equals(MonsterType.KNIGHT))){
+            compareDMG(card1.getDAMAGE(), 0, user1DrawnCard, user2DrawnCard);
+            System.out.println("Water-Spells win against Knights");
+            return true;
+        }else if((card2 instanceof SpellCard && card2.getElement().equals(ElementType.WATER)) && (card1 instanceof MonsterCard && ((MonsterCard) card1).getMonsterType().equals(MonsterType.KNIGHT))){
+            compareDMG(0, card2.getDAMAGE(), user1DrawnCard, user2DrawnCard);
+            System.out.println("Water-Spells win against Knights");
+            return true;
+        }
+        //check if Kraken vs any Spell
+        else if((card1 instanceof MonsterCard && ((MonsterCard) card1).getMonsterType().equals(MonsterType.KRAKEN)) && card2 instanceof SpellCard){
+            compareDMG(card1.getDAMAGE(), 0, user1DrawnCard, user2DrawnCard);
+            System.out.println("Krakens win against Spells");
+            return true;
+        }else if((card2 instanceof MonsterCard && ((MonsterCard) card2).getMonsterType().equals(MonsterType.KRAKEN)) && card1 instanceof SpellCard){
+            compareDMG(0, card2.getDAMAGE(), user1DrawnCard, user2DrawnCard);
+            System.out.println("Krakens win against Spells");
+            return true;
+        }
+        return false;
     }
 
-    //dmg1 - always user1 | dmg2 - always user2
+    //dmg1 - always user1 | dmg2 - always user2 !
     private void  compareDMG(double dmg1, double dmg2, int user1DrawnCard, int user2DrawnCard){
         if (dmg1 > dmg2){
             showWinner(user1, user1DrawnCard, user2DrawnCard);
